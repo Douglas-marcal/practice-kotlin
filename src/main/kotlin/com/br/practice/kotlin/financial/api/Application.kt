e7f6deaa8e2c66b3575183e1a230aaf6110865ee
Douglas Wanderley Fagundes Marcal Do Nascimento <douglasymarcal@gmail.com>
2025-05-13 18:29:34 -0300

    start project

---
+++ src/main/kotlin/com/br/practice/kotlin/financial/api/Application.kt
@@ -0,0 +1,11 @@
+package com.br.practice.kotlin.financial.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
